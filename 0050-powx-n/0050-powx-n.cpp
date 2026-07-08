bdcc70d2591aeaa1153e95daa2b05defce4efdca
harshitgulia00 <harshitgulia599@gmail.com>
2026-07-08 17:49:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.09%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
+    double helper(double x,int n){
+        if(n == 0) return 1;
+        double temp= helper(x,n/2);
+        temp = temp * temp;
+        return (n%2 == 0) ? temp : temp * x;
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        long long exp = abs((long long)n);
+        double result = helper(x,exp);
+        return (n < 0) ? 1.0/result : result;
     }
 };
