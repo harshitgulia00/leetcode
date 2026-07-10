0f44f643a34f2bbe6739ba8ed2ff6ce0836608ae
harshitgulia00 <harshitgulia599@gmail.com>
2026-07-10 12:07:11 +0530

    Time: 150 ms (65.79%), Space: 357.2 MB (56.58%) - LeetHub

--- 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
@@ -1,100 +1,58 @@
 class Solution {
 public:
-    vector<int> pathExistenceQueries(
-        int n,
-        vector<int>& nums,
-        int maxDiff,
-        vector<vector<int>>& queries
-    ) {
-        vector<pair<int, int>> values;
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                     vector<vector<int>>& queries) {
+        vector<pair<int,int>> a;
+        for (int i = 0; i < n; i++) a.push_back({nums[i], i});
+        sort(a.begin(), a.end());
+
+        vector<int> pos(n), comp(n), nxt(n);
+        int g = 0, r = 0;
 
         for (int i = 0; i < n; i++) {
-            values.push_back({nums[i], i});
-        }
+            pos[a[i].second] = i;
+            if (i && a[i].first - a[i - 1].first > maxDiff) g++;
+            comp[i] = g;
 
-        sort(values.begin(), values.end());
-
-        vector<int> position(n);
-        vector<int> component(n);
-
-        int group = 0;
-        component[0] = 0;
-
-        for (int i = 0; i < n; i++) {
-            position[values[i].second] = i;
-
-            if (i > 0 && values[i].first - values[i - 1].first > maxDiff) {
-                group++;
-            }
-
-            component[i] = group;
-        }
-
-        vector<int> next(n);
-        int right = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (right < i) {
-                right = i;
-            }
-
-            while (right + 1 < n &&
-                   values[right + 1].first - values[i].first <= maxDiff) {
-                right++;
-            }
-
-            next[i] = right;
+            r = max(r, i);
+            while (r + 1 < n && a[r + 1].first - a[i].first <= maxDiff) r++;
+            nxt[i] = r;
         }
 
         int LOG = 1;
+        while ((1 << LOG) <= n) LOG++;
 
-        while ((1 << LOG) <= n) {
-            LOG++;
-        }
+        vector<vector<int>> up(LOG, vector<int>(n));
+        up[0] = nxt;
 
-        vector<vector<int>> jump(LOG, vector<int>(n));
-
-        for (int i = 0; i < n; i++) {
-            jump[0][i] = next[i];
-        }
-
-        for (int level = 1; level < LOG; level++) {
+        for (int k = 1; k < LOG; k++) {
             for (int i = 0; i < n; i++) {
-                jump[level][i] = jump[level - 1][jump[level - 1][i]];
+                up[k][i] = up[k - 1][up[k - 1][i]];
             }
         }
 
         vector<int> ans;
-        ans.reserve(queries.size());
 
-        for (int i = 0; i < queries.size(); i++) {
-            int u = position[queries[i][0]];
-            int v = position[queries[i][1]];
+        for (auto &q : queries) {
+            int u = pos[q[0]], v = pos[q[1]];
 
-            if (component[u] != component[v]) {
+            if (comp[u] != comp[v]) {
                 ans.push_back(-1);
                 continue;
             }
 
-            if (u > v) {
-                swap(u, v);
-            }
+            if (u > v) swap(u, v);
 
-            int current = u;
             int steps = 0;
 
-            for (int level = LOG - 1; level >= 0; level--) {
-                if (jump[level][current] < v) {
-                    current = jump[level][current];
-                    steps += (1 << level);
+            for (int k = LOG - 1; k >= 0; k--) {
+                if (up[k][u] < v) {
+                    u = up[k][u];
+                    steps += (1 << k);
                 }
             }
 
-            if (current < v) {
-                steps++;
-            }
-
-            ans.push_back(steps);
+            ans.push_back(steps + (u < v));
         }
 
         return ans;
