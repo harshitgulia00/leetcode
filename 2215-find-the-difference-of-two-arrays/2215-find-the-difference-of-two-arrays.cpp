fbfa69ae7ba2a4e78d7dc0a8a79818694de49d7c
harshitgulia00 <harshitgulia599@gmail.com>
2026-08-16 14:38:20 +0530

    Time: 28 ms (28.12%), Space: 37.6 MB (22.72%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> freq1;
-        unordered_map<int,int> freq2;
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
+
         vector<int> ans1;
         vector<int> ans2;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        //freq1 -> filling for nums1
-        for(int num : nums1){
-            freq1[num]++;
-        }
-
-        //freq2 -> filling for nums2
-        for(int num : nums2){
-            freq2[num]++;
-        }
+        
         //getting distincts of nums1
-        for(int num : nums1){
-            if(!ans1.empty() && ans1[ans1.size()-1] == num) continue;
-            if(!freq2[num]){
+        for(int num : set1){
+            if(!set2.count(num)){
                 ans1.push_back(num);
             }
         }
-        //getting distincts of nums2
-        for(int num : nums2){
-            if(!ans2.empty() && ans2[ans2.size()-1] == num) continue;
-            if(!freq1[num]){
+
+        for(int num : set2){
+            if(!set1.count(num)){
                 ans2.push_back(num);
             }
         }
