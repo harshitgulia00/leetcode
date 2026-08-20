94f94dbe11e20b513aa5e0433c04091f904ac712
harshitgulia00 <harshitgulia599@gmail.com>
2026-08-20 20:55:33 +0530

    Time: 19 ms (56.91%), Space: 28.4 MB (65.89%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     string removeStars(string s) {
-        vector<char> stack;
+
+        string stack = "";
+
         for(char ch : s){
+
             if(ch == '*'){
-                if(!stack.empty()){
-                    stack.pop_back();
-                }
-            }else{
-                    stack.push_back(ch);
+                stack.pop_back();
+            }
+            else{
+                stack.push_back(ch);
             }
         }
-        s = "";
-        for(char c : stack){
-            s+=c;
-        }
-        return s;
+
+        return stack;
     }
 };
